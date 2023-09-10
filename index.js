490c80c6f1158a026503fd0a6c5f50186fc4c0dc
AxelHOOLIC <axelitorous_01_10@hotmail.com>
2023-09-10 12:13:31 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+    // Obtener la referencia de la imagen por su ID
+    var imagen = document.getElementById('miImagen');
+
+    // Cambiar el atributo src de la imagen
+    imagen.src = 'https://i.ibb.co/z41pdgB/Beyblade-Burst-Valt-Aoi-2-1.gif';
+}, 4000);
